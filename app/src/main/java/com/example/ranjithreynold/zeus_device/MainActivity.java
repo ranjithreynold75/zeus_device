8624a8ff814b0a8c522064d7c61cccfa93650176
Vignesh B <vigneshbv02@gmail.com>
2018-03-06 16:22:20 +0530

    Trip plan

--- app/src/main/java/com/example/ranjithreynold/zeus_device/MainActivity.java
+++ app/src/main/java/com/example/ranjithreynold/zeus_device/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.ranjithreynold.zeus_device;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.android.volley.toolbox.Volley.newRequestQueue;
 
 /**
  * Skeleton of an Android Things activity.
@@ -48,6 +63,7 @@ public class MainActivity extends Activity {
 
     TextView otp;
     TextView user;
+    TextView textView;
 
 String car_reg="TN23CA0237";
 String name="";
@@ -57,12 +73,15 @@ String phone="";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView5);
       socket.connect();
       socket.on("message",get_id);
       socket.on("otp",view_otp);
       socket.on("user_connect",user_connect);   //After requesting connection from phone
       //otp=findViewById(R.id.textView);
       user=findViewById(R.id.textView);
+      data_re();
       }
 
 
@@ -139,6 +158,57 @@ String phone="";
     };
 
 
+    public void refresh_data(View view) {
+        data_re();
+    }
 
+    public void data_re()
+    {
+        final String[] data = {""};
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, "http://zeus75.herokuapp.com/trip_plan", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try
+                {
+                    JSONObject jsonObject=new JSONObject(response);
+                    JSONArray jsonArray=jsonObject.getJSONArray("plan");
+                    int count=0;
 
+                    while (count<jsonArray.length())
+                    {
+                        JSONObject jsonObject1=jsonArray.getJSONObject(count);
+                        String user=jsonObject1.getString("user");
+                        String date=jsonObject1.getString("date");
+                        String time=jsonObject1.getString("time");
+                        String description=jsonObject1.getString("description");
+                        data[0] =new String(new StringBuilder(user+"\t"+date+"\t"+time+"\t"+description+"\n"));
+                        count++;
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                textView.setText(data[0]);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if(error instanceof TimeoutError || error instanceof NoConnectionError)
+                {
+                    Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+                }
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> hashMap=new HashMap<>();
+                hashMap.put("id",car_reg);
+                return hashMap;
+            }
+        };
+        RequestQueue requestQueue= newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
 }
